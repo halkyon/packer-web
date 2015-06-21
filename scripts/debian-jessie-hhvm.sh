4a8d5495b02953bf54abf924358acb4e4a0cbd58
Sean Harvey <halkyon@gmail.com>
2015-06-21 12:37:57 +1200

    Update php 5.6 and hhvm to latest versions

--- scripts/debian-jessie-hhvm.sh
+++ scripts/debian-jessie-hhvm.sh
@@ -3,7 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Setup apt repository for HHVM
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
-echo 'deb http://dl.hhvm.com/debian jessie-lts-3.6 main' > /etc/apt/sources.list.d/hhvm.list
+echo 'deb http://dl.hhvm.com/debian jessie main' > /etc/apt/sources.list.d/hhvm.list
 
 # Install software
 apt-get update
